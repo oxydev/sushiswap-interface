dc3920b0ef2aea16864774ae2f772dc1dd23b18c
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2022-02-02 03:11:52 +0300

    oasis integ working

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -25,23 +25,23 @@ export const DEFAULT_LIST_OF_LISTS: string[] =
     process.env.NODE_ENV !== 'production'
         ? []
         : [
-              COMPOUND_LIST,
-              AAVE_LIST,
-              SYNTHETIX_LIST,
-              UMA_LIST,
-              NFTX_LIST,
-              WRAPPED_LIST,
-              SET_LIST,
-              OPYN_LIST,
-              ROLL_LIST,
-              COINGECKO_LIST,
-              CMC_ALL_LIST,
-              CMC_STABLECOIN,
-              KLEROS_LIST,
-              GEMINI_LIST,
-              QUICK_SWAP,
+              // COMPOUND_LIST,
+              // AAVE_LIST,
+              // SYNTHETIX_LIST,
+              // UMA_LIST,
+              // NFTX_LIST,
+              // WRAPPED_LIST,
+              // SET_LIST,
+              // OPYN_LIST,
+              // ROLL_LIST,
+              // COINGECKO_LIST,
+              // CMC_ALL_LIST,
+              // CMC_STABLECOIN,
+              // KLEROS_LIST,
+              // GEMINI_LIST,
+              // QUICK_SWAP,
               ...UNSUPPORTED_LIST_URLS // need to load unsupported tokens as well
           ]
 
 // default lists to be 'active' aka searched across
-export const DEFAULT_ACTIVE_LIST_URLS: string[] = [] //[GEMINI_LIST]
+export const DEFAULT_ACTIVE_LIST_URLS: string[] = []
