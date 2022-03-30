7a63a7d7aefef1c6e3ea6ff8139e3b04930a1602
Saber Zafarpoor <45599139+SaberDoTcodeR@users.noreply.github.com>
2022-03-30 16:43:22 +0300

    graohQL errors fixed except bar

---
+++ src/config/tokens/ethereum.ts
@@ -0,0 +1,10 @@
+import { ChainId, Token } from '@sushiswap/sdk'
+
+// Default Ethereum chain tokens
+export const XSUSHI = new Token(
+  ChainId.OASISETH_MAIN,
+  '0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272',
+  18,
+  'xSUSHI',
+  'SushiBar'
+)
